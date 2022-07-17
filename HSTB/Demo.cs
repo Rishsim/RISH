f57dd6dbb8a7c4daeb6aef0032bd4e98b6dcc772
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-07-17 09:40:19 +0100

    Ht added

---
+++ HSTB/Demo.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSTB
+{
+    public class Demo
+    {
+        public void Z1()
+        {
+            Hashtable ramya = new Hashtable();
+
+            ramya.Add("key", "value");
+
+            ramya.Add(10, 10);
+
+            ramya.Add(887766d,222222);
+
+            foreach (Object suresh in ramya.Values)
+
+            {
+                Console.WriteLine(suresh);
+
+
+            }
+
+
+
+
+        }
+
+
+
+
+    }
+}
---
+++ HSTB/Sample.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HSTB
+{
+    internal class Sample
+    {
+        static void Main(string[] args)
+        {
+            Demo t = new Demo();
+
+            t.Z1();
+
+
+
+
+
+        }
+    }
+
+
+
+        
+}

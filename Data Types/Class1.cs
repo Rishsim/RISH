b0080ec7aa620c201e33de91084f8c8453ed86a4
LAPTOP-EFSKR9KV\ramya <ramya0502@gmail.com>
2022-07-17 09:44:14 +0100

    added datatye folder

---
+++ Data Types/Class1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Data_Types
+{
+    public class Class1
+    {
+        int a = 1;
+        float b = 10.5f;
+        double c = 860200664d;
+        string d = "ramya";
+        char e = 'd';
+        bool f = true;
+    }
+}
